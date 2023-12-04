487f131f38164cddfb81448afc2c9bbf73694018
JesusMartinezG <gsusmag@gmail.com>
2023-12-04 00:13:13 -0600

    Basic map functionality showcase - Added filter selection - Updated view using Bootstrap for element arrangement and styling - Added input to url parsing for calling the api

--- flask_app/webapp/static/map.js
+++ flask_app/webapp/static/map.js
@@ -7,7 +7,7 @@ map.setMinZoom(10);
 // lock map boundries
 map.setMaxBounds([
     [19.660760188533505, -99.41937189586469],
-    [19.058925309976264, -98.88988377708824]
+    [19.058925309976264, -98.8522535751143]
 ]);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -27,73 +27,40 @@ var cfg = {
 }
 
 var apply = document.getElementById('apply');
-var asHeatmap = document.getElementById('asHeatmap');
-var filtersInput = document.getElementById('filters');
-var asCluster = document.getElementById('asCluster');
+// var asHeatmap = document.getElementById('asHeatmap');
+// var filtersInput = document.getElementById('filters');
+// var asCluster = document.getElementById('asCluster');
+
+var keys = ['dia_semana', 'tipo_incidente_c4', 'incidente_c4', 'clas_con_f_alarma', 'tipo_entrada', 'alcaldia_inicio', 'inicio', 'final', 'limit'];
+
+var inputs = {
+    dia_semana: document.getElementById('dia_semana'),
+    tipo_incidente_c4: document.getElementById('tipo_incidente_c4'),
+    incidente_c4: document.getElementById('incidente_c4'),
+    clas_con_f_alarma: document.getElementById('clas_con_f_alarma'),
+    tipo_entrada: document.getElementById('tipo_entrada'),
+    alcaldia_inicio: document.getElementById('alcaldia_inicio'),
+    inicio: document.getElementById('inicio'),
+    final: document.getElementById('final'),
+    limit: document.getElementById('limit')
+}
+// var limitInput = document.getElementById('limit');
 
 apply.addEventListener('click', function() {
-    //get the values from input field
-    filters = filtersInput.value;
-    //if filters is not empty
-    if (filters) {
-        //remove all markers
-        map.eachLayer(function(layer) {
-            if (layer instanceof L.Marker || layer instanceof HeatmapOverlay || layer instanceof L.MarkerCluster) {
-                map.removeLayer(layer);
-            }
-        })
-        //call api endpoint to get coordinates
-        fetch('api/getCoordinatesByFilter/?' + filters)
-            .then(response => response.json())
-            .then(data => {
-                // add data as markers to the map
-                for (var i = 0; i < data.length; i++) {
-                    L.marker([data[i].latitud, data[i].longitud]).addTo(map);
-                }
-            })
-            .catch(error => {
-                // handle any errors that occur during the fetch request
-                console.error(error);
-            });
+    var arguments = [];
+    //get the values from input fields
+    for (var key of keys) {
+        if (inputs[key].value != '') {
+            arguments.push(key + '=' + inputs[key].value);
+        }
     }
 
-})
-
-asHeatmap.addEventListener('click', function() {
-    //get the values from input field
-    filters = filtersInput.value;
-    //if filters is not empty
-    if (filters) {
-        //remove all markers
-        map.eachLayer(function(layer) {
-            if (layer instanceof L.Marker || layer instanceof HeatmapOverlay || layer instanceof L.MarkerCluster) {
-                map.removeLayer(layer);
-            }
-        })
-        //call api endpoint to get coordinates
-        fetch('api/getCoordinatesByFilter/?' + filters)
-            .then(response => response.json())
-            .then(data => {
-                var heatmapData = {
-                    max: data.length,
-                    min: data.length/10,
-                    data: data
-                };
-                cfg.radius= Math.min(5/data.length, 0.006);
-                var heatmapLayer = new HeatmapOverlay(cfg);
-                heatmapLayer.setData(heatmapData);
-                heatmapLayer.addTo(map);
-            })
-            .catch(error => {
-                // handle any errors that occur during the fetch request
-                console.error(error);
-            });
+    // Join the arguments into a single string
+    var filters = '';
+    if (arguments.length > 0) {
+        filters = arguments.join('&');
     }
-})
 
-asCluster.addEventListener('click', function() {
-    //get the values from input field
-    filters = filtersInput.value;
     //if filters is not empty
     if (filters) {
         //remove all markers
@@ -118,7 +85,84 @@ asCluster.addEventListener('click', function() {
                 console.error(error);
             });
     }
+    else{
+        fetch('api/getCoordinatesByFilter/')
+            .then(response => response.json())
+            .then(data => {
+                var markers = L.markerClusterGroup();
+                for (var i = 0; i < data.length; i++) {
+                    var mark = L.marker([data[i].latitud, data[i].longitud]);
+                    markers.addLayer(mark);
+                }
+                markers.addTo(map);
+            })
+            .catch(error => {
+                // handle any errors that occur during the fetch request
+                console.error(error);
+            });
+    }
 })
+
+// asHeatmap.addEventListener('click', function() {
+//     //get the values from input field
+//     filters = filtersInput.value;
+//     //if filters is not empty
+//     if (filters) {
+//         //remove all markers
+//         map.eachLayer(function(layer) {
+//             if (layer instanceof L.Marker || layer instanceof HeatmapOverlay || layer instanceof L.MarkerCluster) {
+//                 map.removeLayer(layer);
+//             }
+//         })
+//         //call api endpoint to get coordinates
+//         fetch('api/getCoordinatesByFilter/?' + filters)
+//             .then(response => response.json())
+//             .then(data => {
+//                 var heatmapData = {
+//                     max: data.length,
+//                     min: data.length/10,
+//                     data: data
+//                 };
+//                 cfg.radius= Math.min(5/data.length, 0.006);
+//                 var heatmapLayer = new HeatmapOverlay(cfg);
+//                 heatmapLayer.setData(heatmapData);
+//                 heatmapLayer.addTo(map);
+//             })
+//             .catch(error => {
+//                 // handle any errors that occur during the fetch request
+//                 console.error(error);
+//             });
+//     }
+// })
+
+// asCluster.addEventListener('click', function() {
+//     //get the values from input field
+//     filters = filtersInput.value;
+//     //if filters is not empty
+//     if (filters) {
+//         //remove all markers
+//         map.eachLayer(function(layer) {
+//             if (layer instanceof L.Marker || layer instanceof HeatmapOverlay || layer instanceof L.MarkerCluster) {
+//                 map.removeLayer(layer);
+//             }
+//         })
+//         //call api endpoint to get coordinates
+//         fetch('api/getCoordinatesByFilter/?' + filters)
+//             .then(response => response.json())
+//             .then(data => {
+//                 var markers = L.markerClusterGroup();
+//                 for (var i = 0; i < data.length; i++) {
+//                     var mark = L.marker([data[i].latitud, data[i].longitud]);
+//                     markers.addLayer(mark);
+//                 }
+//                 markers.addTo(map);
+//             })
+//             .catch(error => {
+//                 // handle any errors that occur during the fetch request
+//                 console.error(error);
+//             });
+//     }
+// })
 
 // //call api endpoint to get coordinates
 // fetch('api/getCoordinatesByFilter/')
