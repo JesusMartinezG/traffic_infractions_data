59c82955b70b356ca388c0c81fffb46873b297b5
JesusMartinezG <gsusmag@gmail.com>
2023-12-21 16:48:16 -0600

    Added route endpoints for new templates

--- flask_app/webapp/routes.py
+++ flask_app/webapp/routes.py
@@ -1,8 +1,17 @@
 from flask import render_template, request, Blueprint
+from datetime import datetime
 
-bp = Blueprint('visualizer', __name__, template_folder='templates',)
+bp = Blueprint('webapp', __name__, template_folder='templates',)
 
 
 @bp.route('/')
 def index():
     return render_template('index.jinja2')
+
+@bp.route('/map')
+def map():
+    return render_template('map.jinja2')
+
+@bp.route('/chart')
+def chart():
+    return render_template('chart.jinja2')
